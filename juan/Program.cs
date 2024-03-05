ee576f5f04e76d06b26c3d187d037208272111f2
Juan Pablo Tapeosi Chamo <52146627+juanpablo45875785@users.noreply.github.com>
2024-03-04 22:04:06 -0400

    Add files via upload

---
+++ juan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+public class juan
+{
+   public static void Main(string[] args)
+   {
+   
+    Console.WriteLine("ingrese un numero:");
+    int numero = Convert.ToInt32(Console.ReadLine());
+    int revernumero = 0;
+    while (numero > 0)
+    {
+    int inversa = numero % 10;
+    revernumero = revernumero * 10 + inversa;
+    numero = numero / 10;
+    }
+    Console.WriteLine("Número invertido: " + revernumero);
+   }
+
+
+
+}
+    
